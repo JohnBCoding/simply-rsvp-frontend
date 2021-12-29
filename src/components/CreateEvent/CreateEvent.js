15370230ce71ea73d75d9ffebe58ae9bf6cf7aea
JohnBCoding <johnbcoding@gmail.com>
2021-12-28 21:07:10 -0500

    Fixed margin issue on create event page

--- src/components/CreateEvent/CreateEvent.js
+++ src/components/CreateEvent/CreateEvent.js
@@ -39,131 +39,133 @@ const CreateEvent = (props) => {
 
     return (
         <>
-            <h1 className="mt-14 md:mt-48 mb-6 md:mb-12 text-3xl md:text-4xl text-secondary">
-                Create New Event
-            </h1>
             {!formSent && (
-                <form
-                    onChange={handleFormData}
-                    onSubmit={handleEventForm}
-                    className="flex flex-col w-3/4 sm:w-1/2 lg:w-3/12"
-                >
-                    <div className="relative border-b-2 border-primary mb-8 z-0 focus-within:border-secondary">
-                        <input
-                            type="text"
-                            name="creator_fn"
-                            placeholder=""
-                            className="block w-full appearance-none focus:outline-none focus:text-secondary bg-transparent"
-                            required
-                        ></input>
-                        <label
-                            htmlFor="creator_fn"
-                            className="absolute bottom-3 origin-0 -z-1 duration-300"
-                        >
-                            First Name
-                        </label>
-                    </div>
-                    <div className="relative border-b-2 border-primary mb-8 z-0 focus-within:border-secondary">
-                        <input
-                            type="text"
-                            name="creator_ln"
-                            placeholder=""
-                            className="block w-full appearance-none focus:outline-none focus:text-secondary bg-transparent"
-                            required
-                        ></input>
-                        <label
-                            htmlFor="creator_ln"
-                            className="absolute bottom-3 origin-0 -z-1 duration-300"
-                        >
-                            Last Name
-                        </label>
-                    </div>
-                    <div className="relative border-b-2 border-primary mb-8 z-0 focus-within:border-secondary">
-                        <input
-                            type="email"
-                            name="creator_email"
-                            placeholder=""
-                            className="block w-full appearance-none focus:outline-none focus:text-secondary bg-transparent"
-                            required
-                        ></input>
-                        <label
-                            htmlFor="creator_email"
-                            className="absolute bottom-3 origin-0 -z-1 duration-300"
-                        >
-                            Email
-                        </label>
-                    </div>
-                    <div className="relative border-b-2 border-primary mb-8 z-0 focus-within:border-secondary">
-                        <input
-                            type="text"
-                            name="event_location"
-                            placeholder=""
-                            className="block w-full appearance-none focus:outline-none focus:text-secondary bg-transparent"
-                            required
-                        ></input>
-                        <label
-                            htmlFor="event_location"
-                            className="absolute bottom-3 origin-0 -z-1 duration-300"
-                        >
-                            Event Location
-                        </label>
-                    </div>
-                    <div className="relative border-b-2 border-primary mt-2 z-0 focus-within:border-secondary">
-                        <input
-                            type="date"
-                            name="event_date"
-                            className="inline-block appearance-none focus:outline-none focus:text-secondary bg-transparent"
-                            required
-                        ></input>
-                        <input
-                            type="time"
-                            name="event_time"
-                            defaultValue={formData["event_time"]}
-                            className="inline-block appearance-none focus:outline-none focus:text-secondary bg-transparent"
-                            required
-                        ></input>
-                        <label
-                            htmlFor="event_date"
-                            className="block absolute bottom-3 origin-0 -z-1 duration-300"
-                        >
-                            Event Date
-                        </label>
-                    </div>
-                    <div className="relative border-b-2 border-primary z-0 focus-within:border-secondary">
-                        <textarea
-                            name="event_desc"
-                            placeholder="Event description"
-                            className="resize-none block placeholder-info w-full h-6 mt-10 appearance-none bg-transparent hover:h-8 focus:outline-none focus:text-secondary focus:h-24 duration-300"
-                            required
-                        ></textarea>
-                        <label
-                            htmlFor="event_desc"
-                            className="absolute top-3 origin-0 -z-1 duration-300"
-                        >
-                            Description
-                        </label>
-                    </div>
-                    <div className="relative border-b-2 border-primary z-0 focus-within:border-secondary">
-                        <textarea
-                            name="event_invites"
-                            placeholder="Add emails seperated by a comma"
-                            className="resize-none block placeholder-info w-full h-6 mt-10 appearance-none bg-transparent hover:h-8 focus:outline-none focus:text-secondary focus:h-24 duration-300"
-                            required
-                        ></textarea>
-                        <label
-                            htmlFor="event_invites"
-                            className="absolute top-3 origin-0 -z-1 duration-300"
-                        >
-                            Send To
-                        </label>
-                    </div>
-                    <button
-                        type="submit"
-                        className="border-4 border-primary p-2 m-4 hover:border-secondary hover:bg-warning duration-700"
+                <>
+                    <h1 className="mt-14 md:mt-48 mb-6 md:mb-12 text-3xl md:text-4xl text-secondary">
+                        Create New Event
+                    </h1>
+                    <form
+                        onChange={handleFormData}
+                        onSubmit={handleEventForm}
+                        className="flex flex-col w-3/4 sm:w-1/2 lg:w-3/12"
                     >
-                        Send
-                    </button>
-                </form>
+                        <div className="relative border-b-2 border-primary mb-8 z-0 focus-within:border-secondary">
+                            <input
+                                type="text"
+                                name="creator_fn"
+                                placeholder=""
+                                className="block w-full appearance-none focus:outline-none focus:text-secondary bg-transparent"
+                                required
+                            ></input>
+                            <label
+                                htmlFor="creator_fn"
+                                className="absolute bottom-3 origin-0 -z-1 duration-300"
+                            >
+                                First Name
+                            </label>
+                        </div>
+                        <div className="relative border-b-2 border-primary mb-8 z-0 focus-within:border-secondary">
+                            <input
+                                type="text"
+                                name="creator_ln"
+                                placeholder=""
+                                className="block w-full appearance-none focus:outline-none focus:text-secondary bg-transparent"
+                                required
+                            ></input>
+                            <label
+                                htmlFor="creator_ln"
+                                className="absolute bottom-3 origin-0 -z-1 duration-300"
+                            >
+                                Last Name
+                            </label>
+                        </div>
+                        <div className="relative border-b-2 border-primary mb-8 z-0 focus-within:border-secondary">
+                            <input
+                                type="email"
+                                name="creator_email"
+                                placeholder=""
+                                className="block w-full appearance-none focus:outline-none focus:text-secondary bg-transparent"
+                                required
+                            ></input>
+                            <label
+                                htmlFor="creator_email"
+                                className="absolute bottom-3 origin-0 -z-1 duration-300"
+                            >
+                                Email
+                            </label>
+                        </div>
+                        <div className="relative border-b-2 border-primary mb-8 z-0 focus-within:border-secondary">
+                            <input
+                                type="text"
+                                name="event_location"
+                                placeholder=""
+                                className="block w-full appearance-none focus:outline-none focus:text-secondary bg-transparent"
+                                required
+                            ></input>
+                            <label
+                                htmlFor="event_location"
+                                className="absolute bottom-3 origin-0 -z-1 duration-300"
+                            >
+                                Event Location
+                            </label>
+                        </div>
+                        <div className="relative border-b-2 border-primary mt-2 z-0 focus-within:border-secondary">
+                            <input
+                                type="date"
+                                name="event_date"
+                                className="inline-block appearance-none focus:outline-none focus:text-secondary bg-transparent"
+                                required
+                            ></input>
+                            <input
+                                type="time"
+                                name="event_time"
+                                defaultValue={formData["event_time"]}
+                                className="inline-block appearance-none focus:outline-none focus:text-secondary bg-transparent"
+                                required
+                            ></input>
+                            <label
+                                htmlFor="event_date"
+                                className="block absolute bottom-3 origin-0 -z-1 duration-300"
+                            >
+                                Event Date
+                            </label>
+                        </div>
+                        <div className="relative border-b-2 border-primary z-0 focus-within:border-secondary">
+                            <textarea
+                                name="event_desc"
+                                placeholder="Event description"
+                                className="resize-none block placeholder-info w-full h-6 mt-10 appearance-none bg-transparent hover:h-8 focus:outline-none focus:text-secondary focus:h-24 duration-300"
+                                required
+                            ></textarea>
+                            <label
+                                htmlFor="event_desc"
+                                className="absolute top-3 origin-0 -z-1 duration-300"
+                            >
+                                Description
+                            </label>
+                        </div>
+                        <div className="relative border-b-2 border-primary z-0 focus-within:border-secondary">
+                            <textarea
+                                name="event_invites"
+                                placeholder="Add emails seperated by a comma"
+                                className="resize-none block placeholder-info w-full h-6 mt-10 appearance-none bg-transparent hover:h-8 focus:outline-none focus:text-secondary focus:h-24 duration-300"
+                                required
+                            ></textarea>
+                            <label
+                                htmlFor="event_invites"
+                                className="absolute top-3 origin-0 -z-1 duration-300"
+                            >
+                                Send To
+                            </label>
+                        </div>
+                        <button
+                            type="submit"
+                            className="border-4 border-primary p-2 m-4 hover:border-secondary hover:bg-warning duration-700"
+                        >
+                            Send
+                        </button>
+                    </form>
+                </>
             )}
 
             <EventDetails
